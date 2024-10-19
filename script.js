88f0d714c9c61fbeaa3c7c97c7a1915405b31e63
Jatoth Adithya Naik <jatothadithyanaik@gmail.com>
2024-10-20 00:07:08 +0530

    added

---
+++ script.js
@@ -0,0 +1,8 @@
+function search(){
+  let userInput =  document.querySelector('input');
+  // console.log(userInput);
+  let text = document.querySelector('a').href="https://www.google.com/search?q="+userInput.value;
+  userInput.value=" ";
+  // let a = document.querySelector('div').
+  
+}
